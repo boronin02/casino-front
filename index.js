7142580f419f1154e095010958781ee9c68fe7ca
Boronin Ivan <boronin02@yandex.ru>
2024-09-22 12:23:49 +0700

    registration added

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const registrationButtonNode = document.querySelector('.button__registration')
 const registrationNode = document.querySelector('.js-registration')
 
+const loginNode = document.querySelector('.login')
+const passwordNode = document.querySelector('.password')
+const registerButtonNode = document.querySelector('.button__register')
+
 registrationButtonNode.addEventListener('click', registrationOpen)
 
 function registrationOpen() {
@@ -11,4 +15,29 @@ registrationNode.addEventListener('click', function (event) {
     if (!event.target.closest('.registration__content')) {
         registrationNode.classList.remove('registration_open')
     }
+})
+
+registerButtonNode.addEventListener('click', function () {
+    let login = loginNode.value
+    let password = passwordNode.value
+
+    const dataToSend = {
+        login,
+        password,
+    };
+
+    fetch('http://127.0.0.1:5000/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(dataToSend)
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Success:', data);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
 })
